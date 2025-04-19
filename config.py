0f1b48010a196a21753c356912dc22d9b987f8b7
Ivan Lloyd Roquero <ivanlloydroquero18@gmail.com>
2025-04-19 20:28:19 +0800

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     DEBUG = False
     DEVELOPMENT = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "your-random-secret-key")
+    SECRET_KEY = os.getenv("SECRET_KEY", "96a56876f636db28072e4a0d92902337dee2a32e327ac3acf5a19897cef56eff")
 
 class ProductionConfig(Config):
     pass
